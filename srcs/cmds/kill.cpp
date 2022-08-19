e7bb7a29a72d66e0fb20335503ef0cbce69b8c19
Lucille Burnet <lburnet@z2r3p7.42lyon.fr>
2022-08-19 13:48:00 +0200

    working on cmds

--- srcs/cmds/kill.cpp
+++ srcs/cmds/kill.cpp
@@ -1,9 +1,17 @@
 #include "cmds.hpp"
 
 int	kill(std::vector<std::string> params, user* askingOne,
-	std::map<unsigned int, user *>& users, Server &srv){
+	std::map<unsigned int, user *>& users, std::vector<channel*> chan_vec,
+	Server &srv){
 	if (askingOne->getLvl() != SRV_OP)
 		return (numeric_reply(ERR_NOPRIVILEGES, askingOne, srv));
-	
+	if (params.size() < 2)
+		return (numeric_reply(ERR_NEEDMOREPARAMS, askingOne, "KILL", srv));
+	user*	usr = NULL;
+	usr = searchUserByNick(params[0], users);
+	if (usr == NULL)
+		return (numeric_reply(ERR_NOSUCHNICK, askingOne, params[0], srv));
+	params.erase(params.begin());
+	quit(params, *usr, chan_vec, srv);
 	return (EXIT_SUCCESS);
 }
