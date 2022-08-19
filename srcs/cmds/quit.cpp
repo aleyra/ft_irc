59cf9f4775c2d56ac70e0894af2cad3252e015df
Tlafay1 <tlclash12@gmail.com>
2022-08-19 14:45:05 +0200

    Fixed for real this time

--- srcs/cmds/quit.cpp
+++ srcs/cmds/quit.cpp
@@ -8,7 +8,7 @@ void	quit(std::vector<std::string> params, user &askingOne,
 		it != chan_vec.end(); ++it)
 	{
 		if ((*it)->getUsr_list().count(askingOne.getId()))
-			(*it)->send(server, "QUIT " + askingOne.getNick() + ": " + params[0]);
+			(*it)->send(server, "QUIT " + askingOne.getNick() + ": " + reason);
 	}
 	error(askingOne, server, "quit requested");
 
