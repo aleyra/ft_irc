cfa9dfb3ad914e32f3d84e08a410815c8841304f
Tlafay1 <tlclash12@gmail.com>
2022-09-08 14:31:17 -0400

    Fixed conflicts with merge

--- srcs/cmds/notice.cpp
+++ srcs/cmds/notice.cpp
@@ -26,8 +26,9 @@ static void	send_msg(Server &server, std::string &message, std::string &target, 
 void	notice(std::vector<std::string> params, user &askingOne,
 			std::vector<channel*> chan_vec,
 			std::map<unsigned int, user *>& users, Server &server)
-{	
+{
 	std::string message = concat(params);
+	int sent = 0;
 
 	if (message.find(':') == std::string::npos)
 		return;
@@ -46,6 +47,7 @@ void	notice(std::vector<std::string> params, user &askingOne,
 			if (receiver == NULL)
 				continue;
 			send_msg(server, message, *it, askingOne, *receiver);
+			sent = 1;
 		}
 		// Channels
 		else
@@ -54,6 +56,9 @@ void	notice(std::vector<std::string> params, user &askingOne,
 			if (chan == NULL)
 				continue;
 			std::map<unsigned int, int> chan_users = chan->getUsr_list();
+			if (chan_users.find(askingOne.getId()) == chan_users.end())
+				continue;
+			
 			if (it->find_first_of("~&@%") != std::string::npos)
 			{
 				int lvl = 0;
@@ -69,7 +74,10 @@ void	notice(std::vector<std::string> params, user &askingOne,
 					it2 != chan_users.end(); it2++)
 				{
 					if (it2->second >= lvl)
+					{
 						send_msg(server, message, *it, askingOne, *searchUserByID(it2->first, users));
+						sent = 1;
+					}
 				}
 
 			}
@@ -77,17 +85,12 @@ void	notice(std::vector<std::string> params, user &askingOne,
 			{
 				for (std::map<unsigned int, int>::iterator it3 = chan_users.begin();
 					it3 != chan_users.end(); it3++)
-
+				{
 					send_msg(server, message, *it, askingOne, *searchUserByID(it3->first, users));
+					sent = 1;
+				}
 
 			}
 		}
 	}
-
-	// Send messages
-	// for (std::vector<user*>::iterator it = recipients.begin();
-	// 	it != recipients.end(); ++it)
-	// {
-	// 	server.send(":" + askingOne.getNick() + "!" + askingOne.getHistory_nick().front() + "@" + askingOne.getIp() + " " + "NOTICE " + (*it)->getNick() + " :" + message, (*it)->getId());
-	// }
 }
