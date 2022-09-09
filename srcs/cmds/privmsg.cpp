e8165adc1b57f16c38448dd8dca0bd66d0861ced
Tlafay1 <tlclash12@gmail.com>
2022-09-09 13:53:25 +0200

    Another fix ?

--- srcs/cmds/privmsg.cpp
+++ srcs/cmds/privmsg.cpp
@@ -101,8 +101,11 @@ void	privmsg(std::vector<std::string> params, user &askingOne,
 				for (std::map<unsigned int, int>::iterator it3 = chan_users.begin();
 					it3 != chan_users.end(); it3++)
 				{
-					send_msg(server, message, *it, askingOne, *searchUserByID(it3->first, users));
-					sent = 1;
+					if (chan->hasMode('m') == false || (chan->hasMode('m') ==  true && it->second >= VOICE_OK))
+					{
+						send_msg(server, message, *it, askingOne, *searchUserByID(it3->first, users));
+						sent = 1;
+					}
 				}
 
 			}
