d280554f118c43d334cbe0fd886339f2757aa4aa
Tlafay1 <tlclash12@gmail.com>
2022-09-10 15:24:20 +0200

    Coding on master again

--- srcs/tools/send.cpp
+++ srcs/tools/send.cpp
@@ -3,7 +3,7 @@
 int	channel::send(user &askingOne, Server &server,
 		std::map<unsigned int, user *> &users, std::string &message, int type)
 	{
-		if (_usr_list.find(askingOne.getId()) == _usr_list.end())
+		if (!this->hasMode('n') && _usr_list.find(askingOne.getId()) == _usr_list.end())
 		{
 			numeric_reply(ERR_CANNOTSENDTOCHAN, &askingOne, this, server);
 			return (0);
@@ -12,23 +12,16 @@ int	channel::send(user &askingOne, Server &server,
 		for (std::map<unsigned int, int>::iterator it = _usr_list.begin();
 			it != _usr_list.end(); ++it)
 		{
-			if (type == PRIVMSG)
-			{
-				if (!this->hasMode('m') || (this->hasMode('m') && _usr_list.at(askingOne.getId()) >= VOICE_OK))
-				{
-					sent = searchUserByID(it->first, users)->send_msg(askingOne,
-						server, message, type, this->getName());
-				}
-				else
-				{
-					numeric_reply(ERR_CANNOTSENDTOCHAN, &askingOne, this, server);
-					break;
-				}
-			}
-			else if (this->hasMode('n'))
+			if (!this->hasMode('m') || (this->hasMode('m') && _usr_list.at(askingOne.getId()) >= VOICE_OK))
 			{
 				sent = searchUserByID(it->first, users)->send_msg(askingOne,
-						server, message, type, this->getName());
+					server, message, type, this->getName());
+			}
+			else
+			{
+				if (type == PRIVMSG)
+					numeric_reply(ERR_CANNOTSENDTOCHAN, &askingOne, this, server);
+				break;
 			}
 		}
 		return (sent);
